61427f56679cb9f6d04afdf0da6d74064b072ac7
LARRYKlNG <aistud3nt@gmail.com>
2023-02-22 20:47:46 -0600

    added test images

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get('/', (req, res) => {
 });
 
 // Start webserver on PORT and log to the console
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
